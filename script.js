3c4dc3855dff14335ec43e4e39194f655a2b65df
Ali Khadembashi <ali.khadembashi@protonmail.com>
2023-01-29 19:09:14 +0330

    feature player UI html css

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+// const volumeUp = document.getElementById('volume-up');
+// const volumeRange = document.querySelector('.vol');
+
+
+
+// volumeUp.addEventListener('click', () => {
+
+// })
