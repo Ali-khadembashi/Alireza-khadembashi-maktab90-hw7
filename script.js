25d625f0335a2c90fb1d204dbea85556e5f0d469
Ali Khadembashi <ali.khadembashi@protonmail.com>
2023-01-29 22:19:45 +0330

    feature html table

--- script.js
+++ script.js
@@ -7,3 +7,31 @@
 // volumeUp.addEventListener('click', () => {
 
 // })
+
+
+// const item = document.querySelector('.scroll-wrap ');
+
+// window.addEventListener('wheel', function (e) {
+
+//     if (e.deltaY > 0) item.scrollLeft += 100;
+//     else item.scrollLeft -= 100;
+// });
+
+
+const db = [
+    {
+        model: '',
+        picture: '',
+        procesor: '',
+        ram: '',
+        hdd: '',
+        display: '',
+        graphics: '',
+        os: '',
+        weight: '',
+        battery: '',
+        price:
+
+
+    }
+];
