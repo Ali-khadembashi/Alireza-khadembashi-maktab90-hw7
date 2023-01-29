e1d94ad1a4c8e4b89c8634869f1a82df21e21182
Ali Khadembashi <ali.khadembashi@protonmail.com>
2023-01-29 18:09:33 +0330

    feature test

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+let map = new Map();
+map.set("name", "John");
+map.set("age", 17);
+map.set("city", "California");
+map.set("maritalStatus", "n/a");
+let keys = map.keys();
+// Error: keys.push is not a function
+// keys.push("more");
+console.log(map.keys());
+
+// for (const iterator of map.keys()) {
+//     console.log(iterator);
+// }
+// const test = Array.from(map.keys());
+// console.log(test);
+
+// console.log(map.entries());
+for (const iterator of map.entries()) {
+    console.log(iterator);
+}
+
+console.log(Array.from(map.entries()));
