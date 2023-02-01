a23ad4a21eaec1624413b67525f82068fb02cf14
Ali Khadembashi <ali.khadembashi@protonmail.com>
2023-02-01 16:23:32 +0330

    feature database

--- script.js
+++ script.js
@@ -17,21 +17,82 @@
 //     else item.scrollLeft -= 100;
 // });
 
+const target = document.getElementById('target');
+
+array.forEach(element => {
+    document.createElement('tbody');
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+    document.createElement('td');
+
+
+});
+
+
+
+
+
 
 const db = [
     {
-        model: '',
-        picture: '',
-        procesor: '',
-        ram: '',
-        hdd: '',
-        display: '',
-        graphics: '',
-        os: '',
-        weight: '',
-        battery: '',
-        price:
+        model: 'HP surface',
+        picture: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWBrzy?ver=85d4&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+        procesor: 'I3 the good one',
+        ram: '3 GB ddr4',
+        hdd: 'SSD 10 GB',
+        display: '12 inches',
+        graphics: 'embedded',
+        os: 'not indlcuded',
+        weight: '200 KG',
+        battery: '2 Hours',
+        price: '$200'
 
 
-    }
-];
+    },
+    {
+        model: 'Azuz Zenbook',
+        picture: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWBrzy?ver=85d4&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+        procesor: 'Intel Core i7 8th Gen',
+        ram: '32 GB RAM',
+        hdd: '512 GB',
+        display: '14 inches',
+        graphics: '3090 GTI',
+        os: 'Win 11',
+        weight: '3.2 KG',
+        battery: '24 hr',
+        price: '$5000'
+
+
+    },
+    {
+        model: 'Leneovo Thinkpad',
+        picture: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RWBrzy?ver=85d4&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true',
+        procesor: 'ADM Ryzen 5 3rd Gen',
+        ram: '2 GB',
+        hdd: '120 GB',
+        display: '16 inches',
+        graphics: 'embedded',
+        os: 'Ununtu 22.4',
+        weight: '1.2 KG',
+        battery: '16 hr',
+        price: '$600'
+
+
+    },
+]
