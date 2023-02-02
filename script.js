b444b352d4620802ab5a90c89d15afb4b244431c
Ali Khadembashi <ali.khadembashi@protonmail.com>
2023-02-02 09:01:24 +0330

    review

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ map.set("maritalStatus", "n/a");
 let keys = map.keys();
 // Error: keys.push is not a function
 // keys.push("more");
-console.log(map.keys());
+// console.log(map.keys());
 
 // for (const iterator of map.keys()) {
 //     console.log(iterator);
@@ -15,9 +15,10 @@ console.log(map.keys());
 // const test = Array.from(map.keys());
 // console.log(test);
 
-// console.log(map.entries());
-for (const iterator of map.entries()) {
-    console.log(iterator);
-}
+// // console.log(map.entries());
+// for (const iterator of map.entries()) {
+//     console.log(iterator);
+// }
 
-console.log(Array.from(map.entries()));
+// console.log(Array.from(map.entries()));
+console.log([...keys]);
